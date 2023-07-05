1ecc61a067c294b40e9f412760e9c2d79f12d338
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2023-07-05 11:51:50 -0400

    Container (#6)

--- .devcontainer/res/scripts/unlock.sh
+++ .devcontainer/res/scripts/unlock.sh
@@ -3,7 +3,7 @@
 pkill -SIGKILL soffice 2>/dev/null || true
 
 # remove the lock file if it exists.
-FILE_LOCK=$CONFIG_DIR/.config/libreoffice/4/.lock
+FILE_LOCK=$HOME/.config/libreoffice/4/.lock
 if [ -f "$FILE_LOCK" ]; then
     rm "$FILE_LOCK"
 fi
